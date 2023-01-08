5282d5214753eae47a26176f231dd02225296862
saif <saif.ali@tcs.com.pk>
2023-01-08 20:11:00 +0500

    both on

--- src/container/navbar/navbar.jsx
+++ src/container/navbar/navbar.jsx
@@ -16,128 +16,130 @@ const Navbar = () => {
     setModalShow(true)
   };
   return (
-    <div style={{ backgroundColor: '#fff' }}>
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <div className="container">
-            <a href="/" style={{
-              display: 'inline-block',
-              paddingTop: '0.3125rem',
-              paddingBottom: '0.3125rem',
-              marginRight: '1rem',
-              fontSize: '1.25rem', lineHeight: 'inherit',
-              whiteSpace: 'nowrap',
-            }} className="navbar-brand nuxt-link-exact-active nuxt-link-active" aria-current="page">
-              <img style={{
-                maxWidth: '100px',
-              }} src={logo} alt="" />
-            </a>
-            <button type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" className="navbar-toggler collapsed">
-              <span className="navbar-toggler-icon">
-              </span>
-            </button>
-            <div id="navbarNav" className="navbar-collapse collapse">
-              <ul className="nav navbar-nav ml-auto" style={{ marginLeft: 'auto' }}>
-                <li onClick={() => modalValueChange("traveler")} className="nav-item"><a data-toggle="modal" className="nav-link custom_nav_link">Travel Login</a></li>
-                <li onClick={() => modalValueChange("agent")} className="nav-item"><a data-toggle="modal" className="nav-link custom_nav_link">Agent Login</a></li>
-                <li onClick={() => modalValueChange("guide")} className="nav-item"><a data-toggle="modal" className="nav-link custom_nav_link">Guide Login</a></li>
-                <li className="burgerIcon">
-                  <Dropdown>
-                    <Dropdown.Toggle id="dropdown-basic">
-                      <AiOutlineMenu />
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      <Dropdown.Item>
-                        <Link to="/find-tour">Find A Tour</Link>
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                        <Link to="/walking-tour">Walking Tour</Link>
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                        <Link to="/bike-tour">Bike Tour</Link>
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                        <Link to="/food-tour">Food Tour</Link>
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </li>
-              </ul>
+    <>
+      <div style={{ backgroundColor: '#fff' }}>
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container">
+              <a href="/" style={{
+                display: 'inline-block',
+                paddingTop: '0.3125rem',
+                paddingBottom: '0.3125rem',
+                marginRight: '1rem',
+                fontSize: '1.25rem', lineHeight: 'inherit',
+                whiteSpace: 'nowrap',
+              }} className="navbar-brand nuxt-link-exact-active nuxt-link-active" aria-current="page">
+                <img style={{
+                  maxWidth: '100px',
+                }} src={logo} alt="" />
+              </a>
+              <button type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" className="navbar-toggler collapsed">
+                <span className="navbar-toggler-icon">
+                </span>
+              </button>
+              <div id="navbarNav" className="navbar-collapse collapse">
+                <ul className="nav navbar-nav ml-auto" style={{ marginLeft: 'auto' }}>
+                  <li onClick={() => modalValueChange("traveler")} className="nav-item"><a data-toggle="modal" className="nav-link custom_nav_link">Travel Login</a></li>
+                  <li onClick={() => modalValueChange("agent")} className="nav-item"><a data-toggle="modal" className="nav-link custom_nav_link">Agent Login</a></li>
+                  <li onClick={() => modalValueChange("guide")} className="nav-item"><a data-toggle="modal" className="nav-link custom_nav_link">Guide Login</a></li>
+                  <li className="burgerIcon">
+                    <Dropdown>
+                      <Dropdown.Toggle id="dropdown-basic">
+                        <AiOutlineMenu />
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu>
+                        <Dropdown.Item>
+                          <Link to="/find-tour">Find A Tour</Link>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                          <Link to="/walking-tour">Walking Tour</Link>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                          <Link to="/bike-tour">Bike Tour</Link>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                          <Link to="/food-tour">Food Tour</Link>
+                        </Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <Login show={modalShow} onHide={() => setModalShow(false)} modalname={modalName} />
+      </div>
+      <div className="navSection">
+        <div className="container">
+          <div className="mainNav">
+            <div className="logo">
+              <Link to="/">
+                <img src={logo} alt="" />
+              </Link>
+            </div>
+            <div className="details">
+              <div className="currency">
+                <Form.Select aria-label="Default select example">
+                  <option value="1">PKR</option>
+                  <option value="2">$ CAD</option>
+                  <option value="3">$ USD</option>
+                </Form.Select>
+              </div>
+              <div className="cart">
+                <BsFillCartFill />
+              </div>
+
+              <div className="loginSignup">
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <BsFillPersonFill />
+                    <span>Log In</span>
+                  </Dropdown.Toggle>
+                  {/* onClick={() => setModalShow(true)} */}
+                  <Dropdown.Menu>
+                    <Dropdown.Item>
+                      <span onClick={() => modalValueChange("traveler")}>
+                        Traveler Login
+                      </span>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <span onClick={() => modalValueChange("agent")}>Agent Login</span>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <span onClick={() => modalValueChange("guide")}>Guide Login</span>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+              <div className="burgerIcon">
+                <Dropdown>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <AiOutlineMenu />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item>
+                      <Link to="/find-tour">Find A Tour</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link to="/walking-tour">Walking Tour</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link to="/bike-tour">Bike Tour</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <Link to="/food-tour">Food Tour</Link>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
             </div>
           </div>
-        </nav>
-      </header>
-      <Login show={modalShow} onHide={() => setModalShow(false)} modalname={modalName} />
-    </div>
-    // <div className="navSection">
-    //   <div className="container">
-    //     <div className="mainNav">
-    //       <div className="logo">
-    //         <Link to="/">
-    //           <img src={logo} alt="" />
-    //         </Link>
-    //       </div>
-    //       <div className="details">
-    //         <div className="currency">
-    //           <Form.Select aria-label="Default select example">
-    //             <option value="1">PKR</option>
-    //             <option value="2">$ CAD</option>
-    //             <option value="3">$ USD</option>
-    //           </Form.Select>
-    //         </div>
-    //         <div className="cart">
-    //           <BsFillCartFill />
-    //         </div>
-
-    //         <div className="loginSignup">
-    //           <Dropdown>
-    //             <Dropdown.Toggle id="dropdown-basic">
-    //               <BsFillPersonFill />
-    //               <span>Log In</span>
-    //             </Dropdown.Toggle>
-    //             {/* onClick={() => setModalShow(true)} */}
-    //             <Dropdown.Menu>
-    //               <Dropdown.Item>
-    //                 <span onClick={()=> modalValueChange("traveler")}>
-    //                   Traveler Login
-    //                 </span>
-    //               </Dropdown.Item>
-    //               <Dropdown.Item>
-    //                 <span onClick={()=> modalValueChange("agent")}>Agent Login</span>
-    //               </Dropdown.Item>
-    //               <Dropdown.Item>
-    //                 <span onClick={()=> modalValueChange("guide")}>Guide Login</span>
-    //               </Dropdown.Item>
-    //             </Dropdown.Menu>
-    //           </Dropdown>
-    //         </div>
-    //         <div className="burgerIcon">
-    //           <Dropdown>
-    //             <Dropdown.Toggle id="dropdown-basic">
-    //               <AiOutlineMenu />
-    //             </Dropdown.Toggle>
-
-    //             <Dropdown.Menu>
-    //               <Dropdown.Item>
-    //                 <Link to="/find-tour">Find A Tour</Link>
-    //               </Dropdown.Item>
-    //               <Dropdown.Item>
-    //                 <Link to="/walking-tour">Walking Tour</Link>
-    //               </Dropdown.Item>
-    //               <Dropdown.Item>
-    //                 <Link to="/bike-tour">Bike Tour</Link>
-    //               </Dropdown.Item>
-    //               <Dropdown.Item>
-    //                 <Link to="/food-tour">Food Tour</Link>
-    //               </Dropdown.Item>
-    //             </Dropdown.Menu>
-    //           </Dropdown>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <Login show={modalShow} onHide={() => setModalShow(false)} modalname = {modalName} />
-    // </div>
+        </div>
+        <Login show={modalShow} onHide={() => setModalShow(false)} modalname={modalName} />
+      </div>
+    </>
   );
 };
 
