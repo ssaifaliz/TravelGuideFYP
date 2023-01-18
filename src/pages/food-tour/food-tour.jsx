a7fdedeb1b0c54c853670d08bd8e9f39dfce51ae
muhammad junaid <muhammad.junaid@ideabox.pk>
2023-01-19 01:58:05 +0500

    food tour ui change

--- src/pages/food-tour/food-tour.jsx
+++ src/pages/food-tour/food-tour.jsx
@@ -8,6 +8,7 @@ import cardImage2 from "../../assets/food_tour/image2.png";
 import cardImage3 from "../../assets/food_tour/image3.png";
 import cardImage4 from "../../assets/food_tour/image4.png";
 import { FaRegClock, FaCheck } from "react-icons/fa";
+import Accordion from "react-bootstrap/Accordion";
 
 const widget = [
   {
@@ -106,7 +107,7 @@ const FoodTour = () => (
       </div>
       <div className="container body">
         <div className="row">
-          <div className="col-3 filters">
+          <div className="col-md-3 filters">
             <div className="date">
               <div className="title">When Are you travelling</div>
               <div className="dateInput">
@@ -135,7 +136,7 @@ const FoodTour = () => (
               </div>
             </div>
           </div>
-          <div className="col-9 cardsSection">
+          <div className="col-md-9 cardsSection">
             <div className="tourCards">
               {cards.map((item) => (
                 <div className="tourCardsContainer">
@@ -159,6 +160,143 @@ const FoodTour = () => (
                   </div>
                 </div>
               ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="faqsSection">
+        <div className="container">
+          <div className="faqs">
+            <div className="heading">Frequently Asked Questions</div>
+            <div className="subHeading">
+              The answers provided below are based on answers previously given
+              by the tour provider to customers’ questions.
+            </div>
+            <div className="accordionPanelFaqs">
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>
+                    <span>Q:</span> What are the best Street Food Tours in
+                    Pakistan?
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body_Ans">
+                      <span>A:</span> The best Street Food Tours in Pakistan
+                      according to Viator travelers are:
+                    </div>
+                    <ul>
+                      <li>Street Food Tour of Lahore's Walled City</li>
+                      <li>Jalebi Khaana Peena Tour</li>
+                      <li>Visit Mohatta Palace Zainab market & Burns road</li>
+                      <li>
+                        Half-Day Karachi Burns Road Private Victoria Ride with
+                        Dinner
+                      </li>
+                      <li>
+                        Ladies Special: Lahore Half day Guided Shopping Tour
+                      </li>
+                    </ul>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>
+                    <span>Q:</span> What Street Food Tours in Pakistan are
+                    taking additional safety precautions?
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body_Ans">
+                      <span>A:</span> The best Street Food Tours in Pakistan
+                      according to Viator travelers are:
+                    </div>
+                    <ul>
+                      <li>Street Food Tour of Lahore's Walled City</li>
+                      <li>Jalebi Khaana Peena Tour</li>
+                      <li>Visit Mohatta Palace Zainab market & Burns road</li>
+                      <li>
+                        Half-Day Karachi Burns Road Private Victoria Ride with
+                        Dinner
+                      </li>
+                      <li>
+                        Ladies Special: Lahore Half day Guided Shopping Tour
+                      </li>
+                    </ul>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>
+                    <span>Q:</span> What Street Food Tours in Pakistan are good
+                    for avoiding crowds?
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <div className="body_Ans">
+                      <span>A:</span> The best Street Food Tours in Pakistan
+                      according to Viator travelers are:
+                    </div>
+                    <ul>
+                      <li>Street Food Tour of Lahore's Walled City</li>
+                      <li>Jalebi Khaana Peena Tour</li>
+                      <li>Visit Mohatta Palace Zainab market & Burns road</li>
+                      <li>
+                        Half-Day Karachi Burns Road Private Victoria Ride with
+                        Dinner
+                      </li>
+                      <li>
+                        Ladies Special: Lahore Half day Guided Shopping Tour
+                      </li>
+                    </ul>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+          </div>
+        </div>
+        <div className="description">
+          <div className="container">
+            <div className="sectionsDescription">
+              <div className="heading">More Tours in Pakistan</div>
+              <div className="text">
+                The answers provided below are based on answers previously given
+                by the tour provider to customers’ questions.
+              </div>
+            </div>
+            <div className="sectionsDescription">
+              <div className="heading">Things to do near Pakistan</div>
+              <div className="text">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of Lorem
+                Ipsum, you need to be sure there isn't anything embarrassing
+                hidden in the middle of text. All the Lorem Ipsum generators on
+                the Internet tend to repeat predefined chunks as necessary,
+                making this the first true generator on the Internet.
+              </div>
+            </div>
+            <div className="sectionsDescription">
+              <div className="heading">Trending attractions</div>
+              <div className="text">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of Lorem
+                Ipsum, you need to be sure there isn't anything embarrassing
+                hidden in the middle of text. All the Lorem Ipsum generators on
+                the Internet tend to repeat predefined chunks as necessary,
+                making this the first true generator on the Internet.
+              </div>
+            </div>
+            <div className="sectionsDescription">
+              <div className="heading">Popular on Viator</div>
+              <div className="text">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by
+                injected humour, or randomised words which don't look even
+                slightly believable. If you are going to use a passage of Lorem
+                Ipsum, you need to be sure there isn't anything embarrassing
+                hidden in the middle of text. All the Lorem Ipsum generators on
+                the Internet tend to repeat predefined chunks as necessary,
+                making this the first true generator on the Internet.
+              </div>
             </div>
           </div>
         </div>
