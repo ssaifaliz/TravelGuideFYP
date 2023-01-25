f0a47a0a4f6c39624e14189d4d34349ebf5e1ccd
muhammad junaid <muhammad.junaid@ideabox.pk>
2023-01-26 00:11:56 +0500

    done

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,6 +11,24 @@ import {
   Footer,
 } from "../../container";
 import "./home.scss";
+import group1 from "../../assets/packages/group3.png";
+import group2 from "../../assets/packages/group4.png";
+import group3 from "../../assets/packages/group5.png";
+import { Link } from "react-router-dom";
+const group = [
+  {
+    image: group1,
+    text: "Quetta zarat (Weekly)",
+  },
+  {
+    image: group2,
+    text: "Ormara Beach (Weekly)",
+  },
+  {
+    image: group3,
+    text: "Gorakh Hills (Weekly)",
+  },
+];
 
 const Home = () => (
   <div>
@@ -20,6 +38,24 @@ const Home = () => (
     <Testimonials />
     <Destination />
     <Explore />
+    <div className="HomePackages">
+      <div className="container">
+        <div className="heading">Tour Packages</div>
+        <div className="row">
+          {group.map((item, index) => (
+            <div className="col-md-4" key={index}>
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+              <h6>{item.text}</h6>
+            </div>
+          ))}
+        </div>
+        <Link to="/tour-packages">
+          <p>See More Packages</p>
+        </Link>
+      </div>
+    </div>
     <Blog />
     <Contactus />
     <Footer />
